a16ea7593ab123743eeb87b46169c10d3b6e00e3
JaniqueM <j.maduray@alustudent.com>
2025-08-16 05:54:03 -0700

    script that prints numbers

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+print(f"Last digit of {number} is (number[-1]") #string prints and inserts the last digit of number 
+if number > than 5:
+    print("is greater than 5")
+elif number == 0:
+    print("and is 0")
+elif number <!= 0:
+    print("and is less than 6 and not 0")
