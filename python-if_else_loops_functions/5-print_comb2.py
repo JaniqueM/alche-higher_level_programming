b31a725241cba01dd6fbdd7c7bcf5d1b2b67073f
JaniqueM <j.maduray@alustudent.com>
2025-08-16 09:08:57 -0700

    updated script

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i != 99:
-        print(f"{i:02}", end=", ")
-    else:
-        print(f"{i:02}")
+    print(f"{i:02}", end=", " if i != 99 else "\n")
 
