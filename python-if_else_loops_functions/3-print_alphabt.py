848fd72d3ce1148cf5598323220fc01b4f4a3ffa
JaniqueM <j.maduray@alustudent.com>
2025-08-16 06:49:54 -0700

    updated script

---
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+for i in range(97, 123):  # ASCII codes for a to z
+    if i == 101 or i == 113:  # skip 'e' (101) and 'q' (113)
+        continue
+    print("{}".format(chr(i)), end="")
