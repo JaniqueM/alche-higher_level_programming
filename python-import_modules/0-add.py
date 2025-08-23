07e3715bddc492cbf7824f33c921f0203992c2a0
JaniqueM <j.maduray@alustudent.com>
2025-08-23 11:38:13 -0700

    updated script

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-def add(a, b):
-    if__name__ == "__main__"
+if __name__ == "__main__":
     from add_0 import add 
 
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a, b)}")
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
