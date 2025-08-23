9c1c63e5e7c5349a24455ddd5bf078de1b9f46f5
JaniqueM <j.maduray@alustudent.com>
2025-08-23 13:36:21 -0700

    program that prints all names of modules

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -5,5 +5,5 @@ if __name__ == "__main__":
     hidden_4 = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(hidden_4)
     for name in sorted(dir(hidden_4)):
-        if not name.startswith(""):
+        if not name.startswith("__"):
             print(name)
