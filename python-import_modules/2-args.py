9236a9ce4e27a20fb293432139e91a06d0911a8a
JaniqueM <j.maduray@alustudent.com>
2025-08-23 12:52:15 -0700

    printing the amount of arguments

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,8 +4,9 @@ if __name__ == "__main__":
     argc = len(sys.argv)-1
     if argc == 1:
         print("1 argument:")
+    elif argc == 0:
+        print("0 arguments.")
     else:
         print("{} arguments:".format(argc))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
-
