f67f23d2aad622c5a3686bb819072ce17d7ddc1a
JaniqueM <j.maduray@alustudent.com>
2025-08-23 08:37:52 -0700

    script for multiples

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,9 +3,9 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz")
-        elif
+        else
             i % 5 == 0:
             print("Buzz")
-        elif
+        else
             i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz")
