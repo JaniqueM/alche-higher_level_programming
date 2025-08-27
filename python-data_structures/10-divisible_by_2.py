8d45d36bc916c0a4c8836ae53598f651550e257f
JaniqueM <j.maduray@alustudent.com>
2025-08-27 00:46:52 -0700

    function that finds all multiples of 2 in a list.

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
+"""Module for max_integer function."""
+
+
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    """Return the biggest integer in a list, or None if the list is empty."""
+    if not my_list:
         return None
 
-    max_val = my_list[0]   # start with the first element
+    max_val = my_list[0]
     i = 1
     while i < len(my_list):
         if my_list[i] > max_val:
